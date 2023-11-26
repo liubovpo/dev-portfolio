ac310e8ad5da306bea4b71c506840fb6bb4205f3
liubovpo <lyubov.html@gmail.com>
2023-11-26 01:41:41 +0100

    routes, navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
-
   return (
-    <>
-        <h1 className="text-3xl font-bold underline text-red-500">
-      Hello world!
-    </h1>
-    </>
-  )
+    <main>
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={'Home'}/>
+          <Route path="/about" element={'About'}/>
+          <Route path="/projects" element={'Projects'}/>
+          <Route path="/contact" element={'Contact'}/>
+        </Routes>
+      </Router>
+    </main>
+  );
 }
 
-export default App
+export default App;
