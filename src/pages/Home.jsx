699311a47c32994125128ef50b3ddb58face86d2
liubovpo <lyubov.html@gmail.com>
2023-11-26 02:00:36 +0100

    canvas,drei,loader

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import {Canvas} from '@react-three/fiber'
+import { Suspense } from 'react'
+import Loader from "../components/Loader";
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className='w-full h-screen relative'>
+      {/* <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
+        Text
+      </div> */}
+      <Canvas 
+      className='w-full h-full bg-transparent'
+      camera={{near:0.1, far:1000}}>
+        <Suspense fallback={<Loader/>}>
+
+        </Suspense>
+      </Canvas>
+    </section>
   )
 }
 
